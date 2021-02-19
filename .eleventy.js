a17cf900848e4a8bd19db4e58418db402a9fb46e
eljeh <edbastarache@gmail.com>
2021-02-19 07:27:58 -0700

    updated file structure & added base template

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,28 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('images')
-  eleventyConfig.addPassthroughCopy('admin')
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('_uploads');
+  eleventyConfig.addPassthroughCopy('_admin');
 
   const {
     DateTime
   } = require("luxon");
 
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-      return DateTime.fromJSDate(dateObj, {
-        zone: 'utc'
-      }).toFormat('yy-MM-dd');
-    });
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {
+      zone: 'utc'
+    }).toFormat('yy-MM-dd');
+  });
 
-    eleventyConfig.addFilter("readableDate", dateObj => {
+  eleventyConfig.addFilter("readableDate", dateObj => {
     return DateTime.fromJSDate(dateObj, {
       zone: 'utc'
     }).toFormat("dd-MM-yy");
   });
 
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+    }
+  };
 };
